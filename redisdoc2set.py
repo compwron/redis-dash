0e6717d0c6e85c26c43843a79a440d661eeb496d
MattK <mksql@yahoo.com>
2012-05-18 19:02:28 -0400

    Title tags added to pages

--- redisdoc2set.py
+++ redisdoc2set.py
@@ -15,13 +15,18 @@ docpaths = ['redis.docset/Contents/Resources/Documents/commands', 'redis.docset/
 
 for docpath in docpaths:
     for fn in os.listdir(docpath):
-        # Add page names 
+        
         fn = fn.strip()
         name = os.path.splitext(fn)[0].strip()
         typ = os.path.split(docpath)[1].strip() 
+        if typ == 'commands': name = name.upper()  # Uppercase command names as in the Redis docs 
         
-        if len(name) > 0 and name != 'whos-using-redis':
-            if typ == 'commands': name = name.upper()  # Uppercase command names as in the Redis docs 
+        # Add title tags to pages
+        html = open('%s/%s' % (docpath, fn),'r').read()
+        html = '<TITLE>Redis: %s</TITLE>\n%s' % (name, html)
+        open('%s/%s' % (docpath, fn),'w').write(html)
+
+        if len(name) > 0 and name not in ('sponsors', 'whos-using-redis'):
             cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'clm', '%s/%s' % (typ,fn)))
             print 'name: %s, path: %s/%s' % (name, typ, fn)
         
