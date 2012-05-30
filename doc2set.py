0eb60e3ca0a2cd1251d1d4229f5ac87c0aca71b6
MattK <mksql@yahoo.com>
2012-05-29 22:07:21 -0400

    Trying to get my git right

--- doc2set.py
+++ doc2set.py
@@ -1,7 +1,6 @@
-#!/usr/local/bin/python
-
+#!/usr/bin/env python
 import os, re, sqlite3
-from bs4 import BeautifulSoup, NavigableString, Tag 
+from bs4 import BeautifulSoup
 
 db = sqlite3.connect('redis.docset/Contents/Resources/docSet.dsidx')
 cur = db.cursor()
